cf66df36db75f8cd8838a0c0f22298c15b6c20a4
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2025-09-10 15:51:07 -0400

    added typescript confiig

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
-import app from "./app.js";
+import app from "./src/app.js";
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({ ok: true, message: "Hello from Node backend 🎉" });
 });
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on Port ${port}!`);
